f73069e1275198ee40f5ec9fd7d1cbc5e2f16bc7
dlw <denglinwen@os-easy.com>
2016-12-23 17:41:29 +0800

    eventfd

---
+++ c/eventfd.c
@@ -0,0 +1,67 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  eventfd.c
+ *
+ *    Description: 父进程创建一个子进程后，休眠2秒，子进程将命令行传入的整型参数写入事件
+ *                  描述符。父进程休眠结束后从描述符中读取数据。
+ *
+ *        Version:  1.0
+ *        Created:  2016年12月23日 17时21分19秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/eventfd.h>
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int main(int argc, char *argv[])
+{
+    int efd, j;
+    uint64_t u;
+    ssize_t s;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <num>...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    efd = eventfd(0, 0);
+    if (efd == -1)
+        handle_error("eventfd");
+
+    switch(fork()) {
+        case 0:
+            for (j = 1; j < argc; j++) {
+                printf("Child writing %s to efd\n", argv[j]);
+                u = strtoull(argv[j], NULL, 0);
+                s = write(efd, &u, sizeof(uint64_t));
+                if (s != sizeof(uint64_t))
+                    handle_error("write");
+            }
+            printf("Child completed write loop\n");
+
+            exit(EXIT_SUCCESS);
+        default:
+            sleep(2);
+            printf("Parent about to read\n");
+            s = read(efd, &u, sizeof(uint64_t));
+            if (s != sizeof(uint64_t))
+                handle_error("read");
+            printf("Parent read %llu (0x%llx) from efd\n",
+                    (unsigned long long) u, (unsigned long long) u);
+            exit(EXIT_SUCCESS);
+        case -1:
+            handle_error("fork");
+    }
+}    
